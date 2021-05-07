76ebd13bdad788decdbce92dc73782756706b44d
Ciprian <trr.cipri@yahoo.com>
2021-05-07 16:51:36 +0300

    added sql table

---
+++ table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Item(
+item_id Varchar(20),
+duration Int
+);
+
+INSERT INTO Item
+VALUES ('aw2af',10);
+
+INSERT INTO Item
+VALUES ('122af',16);
+
+INSERT INTO Item
+VALUES ('www2af',3);
